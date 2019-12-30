c9c17f86210c9af41ffdc5ea0c587a4bffc936b6
Dmytro Krasovsky <kdi@betr.io>
2019-12-30 12:14:24 +0100

    added typedef rule

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 export class App {
   public static main(): void {
     const hello: string = "hello";
-    const world: string = "world";
+    const world = "world";
 
     console.log("%s, %s!", hello, world);
   }
